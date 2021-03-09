9a7ccfa3d8a4419d9da3ac04b179c42a0c5eca77
Sergey Polichnoy <sergey.polichnoy@dataart.com>
2021-03-09 13:32:11 +0300

    review benchmarks and unit tests for SummarizeRange

--- netfuncs.go
+++ netfuncs.go
@@ -4,6 +4,27 @@ import (
 	"net"
 )
 
+// Network represents vanilla IP network.
+// It's represented as CIDR notation.
+type Network = net.IPNet
+
+// Networks is a slice of networks.
+type Networks []*Network
+
+// Strings get all networks as string representation.
+func (nn Networks) Strings() []string {
+	if nn == nil {
+		return nil
+	}
+
+	out := make([]string, 0, len(nn))
+	for _, n := range nn {
+		out = append(out, n.String())
+	}
+
+	return out
+}
+
 // Supernet returns a supernet for the provided network with the specified prefix length
 func Supernet(ipN *net.IPNet, newPrefix int) *net.IPNet {
 	ones, bits := ipN.Mask.Size()
